633ef78fae2a8fc950de87d5403cda98c4483996
YinAqu <gbackmail@gmail.com>
2023-04-29 13:13:48 +0800

    transparent

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,9 +1,12 @@
-lvim.colorscheme = "onedark"
+-- lvim.colorscheme = "onedark"
+lvim.colorscheme = "tokyonight-moon"
+lvim.transparent_window = true
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
 lvim.reload_config_on_save = false
 
-vim.opt.relativenumber = true                 -- set relative numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
+vim.cmd "syntax on"
 -- TODO Not work
 -- vim.opt.foldmethod = "expr"                   -- folding, set to "expr" for treesitter based folding
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
